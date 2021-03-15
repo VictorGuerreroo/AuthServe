cd35ad45269ddea310d6a12ec4260d429128df1e
Victor <v.g.m_@hotmail.com>
2021-03-15 21:10:08 +0100

    Backend Finalizado

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('./db/config');
+require('dotenv').config();
 
+//console.log( process.env);
 
 
 // Crear el servidor/aplicación de express
 const app = express();
 
+// Base de datos
+dbConnection();
+
+// Directorio Público
+app.use( express.static('./public'));
+
+// CORS
+app.use(cors());
+
+// Lectura y parseo del body
+app.use( express.json());
+
 // Rutas
 app.use('/api/auth', require('./routes/auth'));
 
 
-app.listen( 4000, () =>{
-    console.log(`Servidor corriendo en puerto ${ 4000}`);
+app.listen( process.env.PORT, () =>{
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT}`);
 })
